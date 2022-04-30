09ca836dbfdd8ee718eeebaea81bf8cffab81b25
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 11:16:24 +0300

    Add project files.

---
+++ Csharp project  Q4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project__Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("the square root of 12345 is: ");
+            double x = Math.Sqrt(12345);
+            Console.WriteLine(x);
+        }
+    }
+}
